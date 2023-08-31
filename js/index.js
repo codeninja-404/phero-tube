4193f3685ecd89b74fe8a1250da5f0f56c13c5b5
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-09-01 00:23:38 +0600

    settingup song card

--- js/index.js
+++ js/index.js
@@ -4,17 +4,34 @@ const loadData = async () => {
   );
   const dataNest = await res.json();
   const data = dataNest.data;
-  console.log(data.data);
   displaytCategory(data);
 };
 const displaytCategory = (data) => {
   const categoryContainer = document.getElementById("category-container");
   data.forEach((data) => {
     const category = document.createElement("div");
-    category.innerHTML = `<button
+    category.innerHTML = `<button onclick = "displaySong(${data.category_id})"
     class=" text-xs md:text-md font-semibold px-4 py-1 bg-gray-300 rounded-sm hover:bg-red-500 hover:text-white focus:text-white focus:bg-red-500">${data.category}
     </button>`;
     categoryContainer.appendChild(category);
+    // loadSong(data.category_id);
+  });
+};
+
+const displaySong = async (dataId) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${dataId}`
+  );
+  const dataNest = await res.json();
+  const data = dataNest.data;
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+  data.forEach((data) => {
+    const dataCard = document.createElement("div");
+    dataCard.innerHTML = `
+    <p> hello </p>
+    `;
+    cardContainer.appendChild(dataCard);
   });
 };
 
