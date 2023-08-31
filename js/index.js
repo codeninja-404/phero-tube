d10bbb7334102fe5fe83a2dc9a8e7afbc3daa381
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-08-31 22:06:06 +0600

    dynamic category button added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
+const loadData = async () => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/categories`
+  );
+  const dataNest = await res.json();
+  const data = dataNest.data;
+  console.log(data.data);
+  displaytCategory(data);
+};
+const displaytCategory = (data) => {
+  const categoryContainer = document.getElementById("category-container");
+  data.forEach((data) => {
+    const category = document.createElement("div");
+    category.innerHTML = `<button
+    class=" text-xs font-semibold px-4 py-1 bg-gray-300 rounded-sm hover:bg-red-500 hover:text-white focus:text-white focus:bg-red-500">${data.category}
+    </button>`;
+    categoryContainer.appendChild(category);
+  });
+};
 
+loadData();
