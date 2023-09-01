de75bf7bb4e3e0d960c34981e5350bb09e9d8e6f
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-09-01 12:15:22 +0600

    added time

--- js/index.js
+++ js/index.js
@@ -9,8 +9,7 @@ const loadData = async () => {
 };
 const defaultCategory = (data) => {
   const defaultId = data[0].category_id;
-  
-  displaySong(defaultId)
+  displaySong(defaultId);
 };
 const displaytCategory = (data) => {
   const categoryContainer = document.getElementById("category-container");
@@ -32,6 +31,18 @@ const displaySong = async (dataId) => {
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
   data.forEach((data) => {
+    const secoundsStr = data.others.posted_date;
+    const secounds = parseInt(secoundsStr);
+    // console.log(secounds);
+
+    const toHrAndMn = (secounds) => {
+      const minsTotal = Math.floor(secounds / 60);
+      // console.log(minsTotal);
+      const hours = Math.floor(minsTotal / 60);
+      const mins = Math.floor(minsTotal % 60);
+      return { h: hours, m: mins };
+    };
+    const hoursAndMins = toHrAndMn(secounds);
     const dataCard = document.createElement("div");
     dataCard.innerHTML = `
     <div class="card h-full bg-base-100 ">
@@ -41,7 +52,7 @@ const displaySong = async (dataId) => {
                 alt="Shoes"
                 class=" w-full  "
               />
-              <p class="text-xs absolute bottom-2 px-2 py-1 rounded-md bg-black/60 text-white right-4 ">hello</p>
+              <p id="time" class="text-xs absolute bottom-2 px-2 py-1 rounded-md bg-black/60 text-white right-4 "><span>${hoursAndMins.h}</span> hrs <span>${hoursAndMins.m}</span> min ago</p>
             </figure>
             <div class="flex items-start pt-5">
               <div class="avatar">
@@ -60,6 +71,7 @@ const displaySong = async (dataId) => {
             </div>
           </div>
     `;
+
     cardContainer.appendChild(dataCard);
   });
 };
