c17a76a5289e90dd216d88d4c377d0ca7a258e16
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-09-01 09:34:55 +0600

    time added

--- js/index.js
+++ js/index.js
@@ -11,54 +11,15 @@ const displaytCategory = (data) => {
   data.forEach((data) => {
     const category = document.createElement("div");
     category.innerHTML = `<button onclick = "displaySong(${data.category_id})"
-    class=" text-xs md:text-md font-semibold px-4 py-1 bg-gray-300 rounded-sm hover:bg-red-500 hover:text-white focus:text-white focus:bg-red-500">${data.category}
+    class=" text-xs md:text-xl font-semibold px-4 py-1 bg-gray-300 rounded-sm hover:bg-red-500 hover:text-white focus:text-white focus:bg-red-500">${data.category}
     </button>`;
     categoryContainer.appendChild(category);
     return data.category_id;
-  });
-  const displaySong = async (dataId) => {
-    const res = await fetch(
-      `https://openapi.programming-hero.com/api/videos/category/${dataId}`
-    );
-    const dataNest = await res.json();
-    const data = dataNest.data;
-    const cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = "";
-    data.forEach((data) => {
-      const dataCard = document.createElement("div");
-      dataCard.innerHTML = `
-      <div class="card h-5/6 bg-base-100 ">
-              <figure class="w-full h-2/5 rounded-xl">
-                <img
-                  src=${data.thumbnail}
-                  alt="Shoes"
-                  class="w-full rounded-xl "
-                />
-              </figure>
-              <div class="flex items-start pt-5">
-                <div class="avatar">
-                  <div class="w-12 h-12 rounded-full">
-                    <img
-                      src=${data.authors[0].profile_picture}
-                    />
-                  </div>
-                </div>
-                <div class="card-body pt-1 ">
-                  <h2 class="card-title">Building a Winning UX Strategy 
-                    Using the Kano Model</h2>
-                  <p>${data.authors[0].profile_name}</p>
-                  <p>views</p>
-                  
-                </div>
-              </div>
-            </div>
-      `;
-      cardContainer.appendChild(dataCard);
-    });
+  });   
   };
-};
+;
 
-const displaySong = async (dataId) => {
+const displaySong = async (dataId=1000) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/videos/category/${dataId}`
   );
@@ -70,12 +31,13 @@ const displaySong = async (dataId) => {
     const dataCard = document.createElement("div");
     dataCard.innerHTML = `
     <div class="card h-full bg-base-100 ">
-            <figure class="w-full h-2/5 rounded-xl">
+            <figure class=" bg-black h-48 overflow-y-hidden overflow-x-auto rounded-xl relative">
               <img
                 src=${data.thumbnail}
                 alt="Shoes"
-                class="w-full rounded-xl "
+                class=" w-full  "
               />
+              <p class="text-xs absolute bottom-2 px-2 py-1 rounded-md bg-black/60 text-white right-4 ">hello</p>
             </figure>
             <div class="flex items-start pt-5">
               <div class="avatar">
@@ -86,10 +48,9 @@ const displaySong = async (dataId) => {
                 </div>
               </div>
               <div class="card-body pt-1 ">
-                <h2 class="card-title">Building a Winning UX Strategy 
-                  Using the Kano Model</h2>
+                <h2 class="card-title">${data.title}</h2>
                 <p>${data.authors[0].profile_name}</p>
-                <p>views</p>
+                <p>${data.others.views } views</p>
                 
               </div>
             </div>
